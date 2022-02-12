614ab5a799835cb6517d7d5125355a139674fb41
Richard-Cod <richard.bathiebo.7@gmail.com>
2022-02-12 23:00:48 +0100

    e

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,57 @@
 import React from 'react'
-
+import Palette from '../../constants/Palette'
+import Routes from '../../constants/Routes'
 function Footer() {
   return (
-    <div>Footer</div>
+    <div style={{background: Palette.brow , minHeight: "10vh"}} className='px-4 py-10'>
+
+    <div className='md:px-4'>
+
+      <ul style={{color: Palette.yellow}} className='text-center space-y-2 md:text-left shadow-lg py-4 md:flex justify-between'>
+        <p className=''>
+        <a href={Routes.termsAndConditions} className=''>Mobile app</a>
+        </p>
+        <p>
+        <a href={Routes.termsAndConditions} className=''>Our Team</a>
+        </p>
+        <p>
+          <a href={Routes.termsAndConditions} className=''>Our Designer</a>
+        </p>
+        <p>
+          <a href={Routes.termsAndConditions} className=''>
+            <img className='mx-auto' src='/assets/logo.png' />
+          </a>
+        </p>
+        <p>
+          <a href={Routes.termsAndConditions} className=''>Help desk</a>
+        </p>
+        <p>
+          <a href={Routes.termsAndConditions} className=''>Blog</a>
+        </p>
+        <p>
+          <a href={Routes.termsAndConditions} className=''>Terms & Conditions</a>
+        </p>
+        
+
+      </ul>
+
+
+      <div className='socials mt-4 mr-2 text-center py-20'>
+        <div className='flex justify-between max-w-[200px] items-center mx-auto'>
+          <img src='/assets/discord.png' className='w-10 h-8 rounded-full' />
+          <img src='/assets/twitter.png' className='w-10 h-8 rounded-full' />
+          <img src='/assets/youtube.png' className='w-10 h-8 rounded-full' />
+          <img src='/assets/instagram.png' className='w-10 h-8 rounded-full' />
+        </div>
+
+        <p className='text-xs mt-5' style={{color: Palette.yellow}}>
+        © WhiteSlaves, Inc. 2022. See different! It's only digital art
+        </p>
+      </div>
+
+
+    </div>
+    </div>
   )
 }
 
--- src/components/newsLetterPart/NewsLetterPart.js
+++ src/components/newsLetterPart/NewsLetterPart.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import Palette from "../../constants/Palette"
 function NewsLetterPart() {
   return (
-    <div>NewsLetterPart</div>
+    <div style={{background: Palette.brow }} className='pt-20 px-4'>
+      <div className='text-center'>
+        <h1 style={{color: Palette.yellow}} className='underline text-3xl'> Join Us ! </h1>
+
+        <div className='flex flex-row justify-center items-center mt-2'>
+          <div  style={{border: "solid " + Palette.yellow  }} className='border-2 h-8 w-1/2'>
+            <input placeholder='email..' style={{background: Palette.brow}} className='outline-none' />
+          </div>
+          <button style={{color: Palette.brow , background: Palette.yellow , border: "solid 1px"}}  className='px-5 py-1 ml-[-10px]'>send</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
