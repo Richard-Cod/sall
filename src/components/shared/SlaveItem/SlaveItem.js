b3457743988c6a9e037126ca690f96176f7fa605
Richard-Cod <richard.bathiebo.7@gmail.com>
2022-03-03 21:29:28 +0100

    e

--- src/components/shared/SlaveItem/SlaveItem.js
+++ src/components/shared/SlaveItem/SlaveItem.js
@@ -6,7 +6,7 @@ export default function SlaveItem({secondImg}) {
     <div className="space-y-6 flex items-center">
       <img src={`/assets/person${secondImg ?"2" : ""}.png`} className={`w-52 h-44  mt-[20px] ${secondImg ? "rounded-r-xl": ''}`} />
       <div className={`ml-[-45px] mt-[40px] `}>
-      <SlaveCard title="Slaves" />
+      <SlaveCard title={`${secondImg ? "Master" : "Slave"}`} />
       </div>
     </div>
   )
